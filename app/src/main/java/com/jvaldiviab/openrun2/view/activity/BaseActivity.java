a1cb38662c912d54916ccb1250bb5802ab711054
jvaldiviab1 <jvaldiviab@unsa.edu.pe>
2020-11-30 18:25:42 -0500

    actualización login

--- app/src/main/java/com/jvaldiviab/openrun2/view/activity/BaseActivity.java
+++ app/src/main/java/com/jvaldiviab/openrun2/view/activity/BaseActivity.java
@@ -10,27 +10,29 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.jvaldiviab.openrun2.R;
-import com.jvaldiviab.openrun2.databinding.ActivityDeleteBinding;
-import com.jvaldiviab.openrun2.databinding.ActivityRegisterBinding;
+import com.jvaldiviab.openrun2.databinding.ActivityBaseBinding;
+
 
 public class BaseActivity extends AppCompatActivity {
 
-    ActivityDeleteBinding binding;
+    ActivityBaseBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding= ActivityDeleteBinding.inflate(getLayoutInflater());
+        binding = ActivityBaseBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
 
         binding.bottomMenu.inflateMenu(R.menu.menu);
 
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_music, R.id.navigation_profile, R.id.navigation_statistics,R.id.navigation_todos
+                R.id.navigation_music, R.id.navigation_profile, R.id.navigation_statistics, R.id.navigation_todos
         ).build();
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupWithNavController(binding.bottomMenu,navController);
+        NavigationUI.setupWithNavController(binding.bottomMenu, navController);
+
     }
+
 }
