94975e1c5efc879d4ec7ddb165f26085a5a9710f
jvaldiviab1 <jvaldiviab@unsa.edu.pe>
2020-12-03 16:37:13 -0500

    Music Bar and control management

---
+++ app/src/main/java/com/jvaldiviab/openrun2/viewmodel/ProfileViewModel.java
@@ -0,0 +1,15 @@
+package com.jvaldiviab.openrun2.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class ProfileViewModel extends AndroidViewModel {
+
+
+
+    public ProfileViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
